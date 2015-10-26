68e4528b30694838fe341d10a1e58d2f835410b8
BradleyGoldstein <bradgold@umich.edu>
2015-10-26 18:34:26 -0400

    WhaleAnimator is modified with a basic animation sequence

--- Assets/WhaleAnimator.cs
+++ Assets/WhaleAnimator.cs
@@ -4,16 +4,29 @@ using System.Collections;
 
 public class WhaleAnimator : MonoBehaviour {
 
+	private int current_animation;
+	private string[] animations = new string[5];
+
 	void Start()
 	{
-		GetComponent<Animation> ().Play("fastswim");
+		animations [0] = "swim";
+		animations [1] = "fastswim";
+		animations [2] = "death";
+		animations [3] = "fastswim2";
+		animations [4] = "dive";
+
+		current_animation = 0;
+		GetComponent<Animation> ().Play(animations[current_animation]);
 	}
 	
 	void Update() {
-		if (Input.GetMouseButtonDown(0))
-			GetComponent<Animation> ().Play("death");
+		// if the whale died already, can't do another action
+		if (current_animation == 2)
+			return;
+
+		if (Input.GetMouseButtonDown (0))
+			current_animation = Random.Range (0, 5);
+			GetComponent<Animation> ().Play(animations[current_animation]);
 	}
 
 }
-
-

92b33100bfe5df08d1ed942117e84c4a8aa09863
BradleyGoldstein <bradgold@umich.edu>
2015-10-29 16:43:40 -0400

    now works only in reasonable radius of the object

--- Assets/ObjectGrower.cs
+++ Assets/ObjectGrower.cs
@@ -9,43 +9,43 @@ public class ObjectGrower : MonoBehaviour
 	public const float INCREASE_FACTOR = 1.01f;
 	public const float DECREASE_FACTOR = 0.99f;
 	public const float NORMAL_SIZE = 1.0f;
-	Ray ray;
-	RaycastHit hit;
+	public int OBJECT_RADIUS = 50;
+	Vector3 screen_position;
 
-	// Use this for initialization
+
 	void Start()
 	{	
 		currentScale = this.transform.localScale;
+		screen_position = Camera.main.WorldToScreenPoint (this.transform.position);
 	}
 
 	void Update()
 	{
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		double distance_to_mouse = Vector3.Distance(screen_position, Input.mousePosition);
 
-		// If a user is holding down on the object, increase its scale
-		if (Input.GetMouseButton (0))
+		// If the mouse is far away from the object or is not being clicked
+		if (distance_to_mouse > OBJECT_RADIUS || !Input.GetMouseButton(0))
 		{
-			if (Physics.Raycast (ray, out hit))
-			{
-				if (currentScale[0] <= MAX_SIZE)
-				{
-					currentScale = currentScale * INCREASE_FACTOR;
-				}
-			}
-		} 
-
-		// Otherwise, return the scale of the object back to the standard size
-		else
-		{
-			if (currentScale[0] > NORMAL_SIZE)
-			{
+			// If the object is bigger than normal, shrink it down
+			if (currentScale [0] > NORMAL_SIZE) {
 				currentScale = currentScale * DECREASE_FACTOR;
 			}
-			else
-			{
-				currentScale = new Vector3(NORMAL_SIZE, NORMAL_SIZE, NORMAL_SIZE);
+			// Or set it to the standar size
+			else {
+				currentScale = new Vector3 (NORMAL_SIZE, NORMAL_SIZE, NORMAL_SIZE);
 			}
 		}
+		// If the mouse is close to the object and clicking
+		else
+		{
+			// If the object isn't too big, increase it's size.
+			if (currentScale[0] <= MAX_SIZE)
+			{
+				currentScale = currentScale * INCREASE_FACTOR;
+			}
+		}
+
+		// Set the new object to its new scale
 		this.transform.localScale = currentScale;
 
 	}

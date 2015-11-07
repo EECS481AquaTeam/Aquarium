4790a64e52233753ad52caffa7a8a87835060050
ndubovit <ndubovitsky1@gmail.com>
2015-11-07 17:21:43 -0500

    Code for boxes that line up in a row

--- Assets/Fish.cs
+++ Assets/Fish.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fish : MonoBehaviour {
+public enum fishState {NORMAL, MOVINGTO};
 
+
+public class Fish : MonoBehaviour {
+	public static int count = 0;
+	public Vector3 dest = new Vector3(-5,0,0);
 	// Speed of a Fish at a givien time (x,y,z) components
-	private Vector3 speed = new Vector3(Random.Range (-8, 8),
-	                                    Random.Range (-8, 8),
-	                                    Random.Range (-8, 8));
+	private Vector3 speed = new Vector3(Random.Range (-3, 3),
+	                                    Random.Range (-3, 3),
+	                                    Random.Range (-3, 3));
+
+	fishState fState = fishState.NORMAL;
 
 	// Create a random initial location of the fist
 	void Start () {
 
 		Vector3 tempPos = pos;
-		tempPos.x = Random.Range (-50, 50);
-		tempPos.y = Random.Range (-50, 50);
-		tempPos.z = Random.Range (-50, 50);
+		tempPos.x = Random.Range (-20, 20);
+		tempPos.y = Random.Range (-20, 20);
+		tempPos.z = Random.Range (-20, 20);
 		pos = tempPos;
 
-//Should make this a static public variable, but couldn't figure out how to do this with colors
+//TODO: Should make this a static public variable, but couldn't figure out how to do this with colors
 //For now keep the less efficient:
 		string[] colors = {"red", "green", "blue"};
 //But afterwards, change to Color[] colors = {Color.red,....};
@@ -33,22 +39,29 @@ public class Fish : MonoBehaviour {
 			gameObject.GetComponent<Renderer>().material.color = Color.blue;
 		}
 
+	}
+
+	void Update() {
+		switch (fState) {
+		case fishState.NORMAL:
+			move();
+			break;
+		case fishState.MOVINGTO:
+			moveTo();
+			break;
 		}
-	
-	// Update is called once per frame
-	void Update () {
-		Move ();
 	}
 
-	// When the user clicks a fish, give it a new, random speed
 	void OnMouseDown() {
-		speed[0] = Random.Range (-10, 10);
-		speed[1] = Random.Range (-10, 10);
-		speed[2] = Random.Range (-10, 10); 
+		fState = fishState.MOVINGTO;
+		dest.x = dest.x + (2 * count);
+		count ++;
+//		speed[0] = Random.Range (-10, 10);
+//		speed[1] = Random.Range (-10, 10);
+//		speed[2] = Random.Range (-10, 10); 
 	}
 
-	// Update the location by 1 unit of time
-	public virtual void Move() {
+	public virtual void move() {
 		Vector3 tempPos = pos;
 		tempPos.x += speed[0] * Time.deltaTime;
 		tempPos.y += speed[1] * Time.deltaTime;
@@ -56,6 +69,10 @@ public class Fish : MonoBehaviour {
 		pos = tempPos;
 	}
 
+	public virtual void moveTo() {
+		pos = Vector3.MoveTowards (pos, dest, .2f);
+	}
+		
 	// Current position of the Fish
 	public Vector3 pos {
 		get {
@@ -65,4 +82,5 @@ public class Fish : MonoBehaviour {
 			this.transform.position = value;
 		}
 	}
+
 }

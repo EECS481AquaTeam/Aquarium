a67d5c6a72bb67553d1181f5a1d20b16972af6cd
BradleyGoldstein <bradgold@umich.edu>
2015-11-08 22:27:21 -0500

    base class for all objects added

---
+++ Assets/ActionObject.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionObject : MonoBehaviour {
+	public static float INCREASE_FACTOR = 1.01f;// The rate at which the object grows
+	public static float DECREASE_FACTOR = 0.99f;// The rate at which the object shrinks
+	public static float NORMAL_SIZE = 1.0f;		// The normal scale of an object
+	public static int OBJECT_RADIUS = 50;		// How far around an object is considered touching the object
+	public static double EQUAL_VECTORS = 0.01;	// 3d vectors are considered to be equal if the magnitude of their differences < EQUAL_VECTORS
+
+	// Speed of a Fish at a given time (x,y,z) components
+	private Vector3 speed;
+	
+	void Awake()
+	{
+		pos = GetRandomVector (30);
+		speed = GetRandomVector (8);
+
+		Debug.Log ("created");
+	}
+
+	// Destroy the object if it is outside the frame of the camera
+	void Update()
+	{
+		if (OutsideCamera ()) {
+			Destroy (gameObject);
+			Debug.Log ("destroyed");
+		}
+	}
+
+	// The object grows if the mouse is clicking the object, and shrinks back to its normal size otherwize 
+	void Grow ()
+	{
+		scale = scale * INCREASE_FACTOR;
+	}
+
+	void Shrink ()
+	{
+		scale = scale * DECREASE_FACTOR;
+	}
+
+	void ResetScale()
+	{
+		scale = new Vector3 (NORMAL_SIZE, NORMAL_SIZE, NORMAL_SIZE);
+	}
+
+	// Update the location by 1 unit of time
+	void Move() {
+		Vector3 tempPos = pos;
+		tempPos.x += speed[0] * Time.deltaTime;
+		tempPos.y += speed[1] * Time.deltaTime;
+		tempPos.z += speed[2] * Time.deltaTime;
+		pos = tempPos;
+	}
+
+	void MoveTowardsTarget( Vector3 targetPosition) {
+
+		Vector3 directionOfTravel = targetPosition - pos;
+
+		//now normalize the direction, since we only want the direction information
+		directionOfTravel.Normalize();
+
+		//scale the movement on each axis by the directionOfTravel vector components
+		this.transform.Translate(
+			(directionOfTravel.x * speed * Time.deltaTime),
+			(directionOfTravel.y * speed * Time.deltaTime),
+			(directionOfTravel.z * speed * Time.deltaTime),
+			Space.World);
+	}
+	
+	Vector3 GetRandomVector(int range=10)
+	{
+		return new Vector3(Random.Range (-range, range),
+		                   Random.Range (-range, range),
+		                   Random.Range (-range, range));
+	}
+	
+	Vector3 PositionOnScreen()
+	{
+		return Camera.main.WorldToScreenPoint (pos);
+	}
+
+	bool ClickedOn()
+	{
+		// mouse is not being clicked
+		if (!Input.GetMouseButton (0))
+			return false;
+
+		// Find distance that the mouse is from the object on the screen
+		double distance_to_mouse = Vector3.Distance(PositionOnScreen, Input.mousePosition);
+
+		// Mouse is clicking far from the object
+		if (distance_to_mouse > OBJECT_RADIUS)
+			return false;
+		// Mouse is clicking close to the object
+		else
+			return true;
+	}
+	
+	bool OutsideCamera()
+	{
+		if ((pos.x > 15 || pos.x < -15) || (pos.y > 15 || pos.y < -15))
+			return true;
+		else
+			return false;
+	}
+	
+	// Defines vectors to be equal if the magnitude of their difference is sufficiently small 
+	static public bool V3Equal(Vector3 a, Vector3 b)
+	{
+		return Vector3.SqrMagnitude(a - b) < EQUAL_VECTORS;
+	}
+	
+	// Getter & setter for scale of the object
+	public Vector3 scale
+	{
+		get {
+			return (this.transform.localScale);
+		} set {
+			this.transform.localScale = value;
+		}
+	}
+	
+	// Getter & setter for scale of the object
+	public Vector3 pos {
+		get {
+			return (this.transform.position);
+		}
+		set {
+			this.transform.position = value;
+		}
+	}
+}

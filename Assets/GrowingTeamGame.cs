4daabb1778f88d14f7986908edd224df3354c2b5
BradleyGoldstein <bradgold@umich.edu>
2015-11-15 18:42:43 -0500

    restructure growingTeamGame.cs

--- Assets/GrowingTeamGame.cs
+++ Assets/GrowingTeamGame.cs
@@ -2,103 +2,142 @@ using UnityEngine;
 using System.Collections;
 using System;
 
-public enum turn {LEFT, RIGHT};
+public enum turn {LEFT, RIGHT, START};
 
 public class GrowingTeamGame : MonoBehaviour
 {
-	public int beats = 100; // how many times the 
-
 	public static float WINNER_SCALE = 2.0f;
+
+	turn turnState = turn.START;
 	
-	public GameObject left;
-	public GameObject right;
-	turn turnState = turn.LEFT;
+	public GameObject left;		// the left fish in the game
+	public GameObject right;	// the right fish in the game
 
-	public AudioClip negative;
-	public AudioClip looping;
-	public AudioClip positive;
-	public AudioSource audio;
+	public AudioClip negative;	// a clip of music with negative feedback
+	public AudioClip looping;	// a clip of music with background music
+	public AudioClip positive;	// a clip of music with positive feedback
+	public new AudioSource audio;
 
-	public DateTime last = DateTime.Now;
+	public DateTime last;		// the last time that a fish has grown or shrunk
 	
 	void Start() {
 		left = Instantiate (left);
-		left.GetComponent<ActionObject> ().Initialize (new Vector3(-5,1,1), 0f);
+		InitializeFish (left, new Vector3 (-5, 1, 1));
 		right = Instantiate (right);
-		right.GetComponent<ActionObject> ().Initialize (new Vector3(5,1,1), 0f);
+		InitializeFish (right, new Vector3 (5, 1, 1));
 
 		audio = GetComponent<AudioSource>();
+
 		SetRegularAudio ();
+		UpdateTime ();
+	}
+
+	void InitializeFish(GameObject item, Vector3 location)
+	{
+		item.GetComponent<ActionObject>().Initialize(location, 0f);
 	}
 
 	void Update() {
-		if (!audio.isPlaying)
-		{
-			SetRegularAudio ();
-		}
 
-		ActionObject grower = (turnState == turn.LEFT) ? left.GetComponent<ActionObject> () : right.GetComponent<ActionObject> ();
-		ActionObject shrinker = (turnState == turn.RIGHT) ? left.GetComponent<ActionObject> () : right.GetComponent<ActionObject> ();
+		// Attempt to set the default audio
+		SetRegularAudio ();
 
+		// If it hasn't been half a second since the last action, don't do anything
 		if ((DateTime.Now - last).TotalSeconds < 0.5f)
 			return;
 
-		// If the proper whale is clicked on, grow it
-		if (grower.ClickedOn ()) {
-			SetPositiveAudio();
-
-			grower.Grow (1.05f);
-			turnState = (turnState == turn.LEFT) ? turn.RIGHT : turn.LEFT;
-			StartCoroutine(Wait(2.0f));
-
-			last = DateTime.Now;
-		}
-
-		// If the improper whale is clicked on, shrink both whales
-		else if (shrinker.ClickedOn ()) {
-
-			SetNegativeAudio();
-
-			if (shrinker.scale[0] > 0.5)
-			{
-				shrinker.Grow (0.95f);
-			}
-			if (grower.scale[0] > 0.5)
-			{
-				grower.Grow (0.95f);
+		// If a fish has already been clicked, 
+		if (turnState != turn.START) {
+			ActionObject grower = (turnState == turn.LEFT) ? left.GetComponent<ActionObject> () : right.GetComponent<ActionObject> ();
+			ActionObject shrinker = (turnState == turn.RIGHT) ? left.GetComponent<ActionObject> () : right.GetComponent<ActionObject> ();
+		
+			// If the proper whale is clicked on, grow it
+			if (grower.ClickedOn ()) {
+				Grow (grower);
+				turnState = (turnState == turn.LEFT) ? turn.RIGHT : turn.LEFT;
 			}
 
-			last = DateTime.Now;
-		}
+			// If the improper whale is clicked on, shrink both whales
+			else if (shrinker.ClickedOn ()) {
+				Shrink (grower, shrinker);
+			}
 
-		if (grower.scale[0] > WINNER_SCALE && shrinker.scale[0] > WINNER_SCALE) {
-			//end the game with a reward and move back to regular mode
+			if (grower.scale [0] > WINNER_SCALE && shrinker.scale [0] > WINNER_SCALE) {
+				//end the game with a reward and move back to regular mode
+			}
+		}
+		else
+		{
+			ActionObject left_ob = left.GetComponent<ActionObject> ();
+			ActionObject right_ob = right.GetComponent<ActionObject> ();
+			if (left_ob.ClickedOn())
+			{
+				Grow(left_ob);
+				turnState = turn.RIGHT;
+			}
+			else if (right_ob.ClickedOn())
+			{
+				Grow(right_ob);
+				turnState = turn.LEFT;
+			}
 		}
 	}
 
 	void SetRegularAudio()
 	{
-		audio.clip = looping;
-		audio.loop = false;
-		audio.volume = 0.5f;
-		audio.Play ();
+		// if the clip is not playing, set it to the standard audio
+		if (!audio.isPlaying)
+		{
+			audio.clip = looping;
+			audio.loop = true;
+			audio.volume = 0.5f;
+			audio.Play ();
+		}
 	}
-
-	void SetPositiveAudio()
+	
+	void SetFeedbackAudio(AudioClip clip)
 	{
-		audio.loop = false;
-	}
+		// if the clip is already playing, do nothing
+		if (audio.clip == clip && audio.isPlaying)
+		{
+			return;
+		}
 
-	void SetNegativeAudio()
-	{
-		audio.clip = negative;
+		audio.clip = clip;
 		audio.loop = false;
 		audio.volume = 0.75f;
 		audio.Play ();
 	}
 
-	IEnumerator Wait(float duration=5.0f)
+	void Grow(ActionObject item)
 	{
-		yield return new WaitForSeconds(duration);   //Wait
+		SetFeedbackAudio (positive);
+		
+		item.Grow (1.05f);
+		
+		UpdateTime ();
+	}
+
+	void Shrink(ActionObject item1, ActionObject item2)
+	{
+		SetFeedbackAudio (negative);
+		
+		ShrinkHelper (item1);
+		ShrinkHelper (item2);
+		
+		UpdateTime ();
+	}
+
+	void ShrinkHelper(ActionObject item)
+	{
+		if (item.scale [0] > 0.5)
+		{
+			item.Grow (0.95f);
+		}
+	}
+
+	void UpdateTime()
+	{
+		last = DateTime.Now;
 	}
 }

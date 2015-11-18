eff957eee0a1dc58732e1f5d6d0794ec9a84875c
ndubovit <ndubovitsky1@gmail.com>
2015-11-17 21:42:43 -0500

    Added aquarium game and incorporated Utility.cs

---
+++ Assets/AquariumGame.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whaleClass {
+	public GameObject whale;
+	public Vector3 targetPos;
+	public bool targetReached;
+	public whaleClass(GameObject w, Vector3 targetPosition) {
+		whale = w;
+		targetPos = targetPosition;
+		targetReached = false;
+	}
+}
+
+public class AquariumGame : MonoBehaviour {
+	public static int count = 0;
+	public static int numObjects = 7; 
+	public static int lineCount = 0;
+	
+	public Vector3 targetPos;
+	public Vector3 offscreenPos = new Vector3 (-12,0,0);
+	public Vector3 onscreenPos;
+	public Vector3 startPos = new Vector3(12,3,1);
+	
+	public new AudioSource audio;
+	public new AudioClip positive;
+	public static bool audioIsPlaying = true;
+	
+	public bool end = false;
+	
+	public GameObject[] ws2;
+	
+	public whaleClass[] whaleList2 = new whaleClass[7];
+	
+	void Start() {
+		if (GetComponent<Main> ().enabled)
+			GetComponent<Main> ().enabled = false;
+		
+		audio = GetComponent<AudioSource> ();
+		Debug.Log ("Start");
+		for (int i = 0; i < numObjects; ++i) {
+			whaleList2 [i] = new whaleClass (Instantiate(ws2[i]), targetPos);
+			Debug.Log ("Instantiate whale"); 
+		}
+		foreach (whaleClass w in whaleList2) {
+			w.whale.GetComponent<Whale>().MoveTowardsTarget(offscreenPos);
+		}
+	}
+
+	void OnEnable()
+	{
+		Debug.Log ("Aquarium Game Enabled");
+		audioIsPlaying = false;
+		if (GetComponent<Main>().enabled)
+			GetComponent<Main>().enabled = false;
+		
+		foreach (whaleClass w in whaleList2) {
+			//w.whale.GetComponent<Whale>().Initialize(startPos, 0.5f);
+			offscreenPos = Utility.GetRandomVector(3);
+			offscreenPos.x = -12f;
+			w.whale.GetComponent<Whale>().MoveTowardsTarget(offscreenPos);
+			//startPos.y = startPos.y-1;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		foreach (whaleClass w in whaleList2) {
+			if (w.whale.GetComponent<Whale>().ClickedOn()) {
+				audioIsPlaying = false;
+				SetFeedbackAudio();
+			}
+			if (Utility.V3Equal(w.whale.GetComponent<Whale>().pos, offscreenPos)) {
+				w.targetReached = true;
+			}
+			if (w.targetReached) {
+				lineCount++;
+			}
+		}
+		if (lineCount == numObjects) {
+			GetComponent<AquariumGame> ().enabled = false;
+			GetComponent<Main> ().enabled = true;
+		}
+		lineCount = 0;
+		Debug.Log ("Increment line count" + lineCount);
+	}
+
+	void SetFeedbackAudio() {
+		if (!audioIsPlaying) {
+			print ("play sound");
+			audio.clip = positive;
+			audio.loop = false;
+			audio.Play ();
+			audioIsPlaying = true;
+		}
+	}
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class Utility {
+	private const double EQUAL_VECTORS = 0.01;	// 3d vectors are considered to be equal if the magnitude of their differences < EQUAL_VECTORS
+	
+	// Initializes the location of a fish
+	static public void InitializeFish(GameObject item, Vector3 location)
+	{
+		item.GetComponent<ActionObject>().Initialize(location, 5f);
+	}
+	
+	static public void MoveHelper(GameObject g1, Vector3 v1, GameObject g2, Vector3 v2)
+	{
+		g1.GetComponent<ActionObject> ().MoveTowardsTarget(v1);
+		g2.GetComponent<ActionObject> ().MoveTowardsTarget(v2);
+	}
+	
+	static public void MoveHelper(GameObject g1, Vector3 v1, GameObject g2, Vector3 v2, GameObject g3, Vector3 v3)
+	{
+		MoveHelper (g1, v1, g2, v2);
+		g3.GetComponent<ActionObject> ().MoveTowardsTarget(v3);
+	}
+	
+	// Defines vectors to be equal if the magnitude of their difference is sufficiently small 
+	static public bool V3Equal(Vector3 a, Vector3 b)
+	{
+		return Vector3.SqrMagnitude(a - b) < EQUAL_VECTORS;
+	}
+	
+	// Returns a 3d vector with random values in the x, y and z plains within the given range
+	static public Vector3 GetRandomVector(int range=8)
+	{
+		return new Vector3(Random.Range (-range, range),
+		                   Random.Range (-3, 3),
+		                   Random.Range (0, 5));
+	}
+}

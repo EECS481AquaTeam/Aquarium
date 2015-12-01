3eb1e1e5f1a6b316c24eca0d29b1d42aa1ce1ccc
BradleyGoldstein <bradgold@umich.edu>
2015-12-01 09:26:24 -0500

    Protect game working

--- Assets/ProtectGame.cs
+++ Assets/ProtectGame.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class AttackerWithTarget {
+	public GameObject attacker;
+	public Vector3 target;
+	public AttackerWithTarget(GameObject a, Vector3 targetPosition) {
+		attacker = a;
+		target = targetPosition;
+	}
+}
+
 public class ProtectGame : MonoBehaviour {
 //	public new AudioSource audio;
 //	public AudioClip positive;
@@ -10,71 +19,63 @@ public class ProtectGame : MonoBehaviour {
 	public GameObject defender;
 	private int health = 10;
 
-	public GameObject[] attackers;
-	public Vector3[] destinations;
+	public GameObject[] attacks;
+	AttackerWithTarget[] attackers = new AttackerWithTarget[10];
 	
 	void Start() {
 //		audio = GetComponent<AudioSource> ();
 		defender = Instantiate (defender);
 		Utility.InitializeFish (defender, targetPos, 0);
 
-		if (attackers.Length != destinations.Length) {
-			Debug.LogError("Error: The number of attackers does not equal the number attacker destinations");
-			return;
-		}
-
-
 		for (int i = 0; i < attackers.Length; ++i) {
-			attackers[i] = Instantiate(attackers[i]);
-			Utility.InitializeFish(attackers[i], new Vector3(Random.Range (-100,100), Random.Range (-100,100), 1), Random.Range (1,2));
-			attackers[i].GetComponent<ActionObject>().MakeUndestroyable();
-		}
+			attackers [i] = new AttackerWithTarget (Instantiate (attacks [i]), targetPos);
 
-		for (int i = 0; i < destinations.Length; ++i) {
-			destinations[i] = targetPos;
+			GameObject attacker = attackers [i].attacker;
+			Utility.InitializeFish (attacker, new Vector3 (Random.Range (-100, 100), Random.Range (-100, 100), 1), Random.Range (5, 10));
+			attacker.GetComponent<ActionObject> ().MakeUndestroyable ();
 		}
 	}
 
 	void Update () {
 		bool attackersRemaining = false;
-		for (int i = 0; i < attackers.Length; ++i)
-		{
-			if (!attackers[i]) continue;
-			else attackersRemaining = true;
-
-			ActionObject script = attackers[i].GetComponent<ActionObject>();
-
-			if (Utility.V3Equal(script.pos, destinations[i]))
-			{
-				if (Utility.V3Equal(script.pos, targetPos))
-				{
-					--health;
-					Debug.Log("hit!");
-				}
-				Destroy(attackers[i]);
-				attackers[i] = null;
-			}
+		for (int i = 0; i < attackers.Length; ++i) {
+			if (!attackers [i].attacker)
+				continue;
 			else
-			{
-				if (script.ClickedOn() && Utility.V3Equal(destinations[i], targetPos))
-				{
-					destinations[i] = GetNewTarget(script);
+				attackersRemaining = true;
+
+			GameObject attacker = attackers [i].attacker;
+
+			ActionObject script = attacker.GetComponent<ActionObject> ();
+
+			if (Utility.V3Equal (script.pos, attackers [i].target)) {
+				if (Utility.V3Equal (script.pos, targetPos)) {
+					--health;
+					print (string.Format ("Hit! {0} health remaining.", health));
+					Destroy (attacker);
+					attackers [i].attacker = null;
+				} else {
+					attackers [i].target = targetPos;
+				}
+
+			} else {
+				if (script.ClickedOn () && Utility.V3Equal (attackers [i].target, targetPos)) {
+					attackers [i].target = GetNewTarget (script);
 				} 
-				script.MoveTowardsTarget(destinations[i]);
+				script.MoveTowardsTarget (attackers [i].target);
 			}
 		}
 
-		if (!attackersRemaining)
-		{
+		if (!attackersRemaining) {
 			enabled = false;
 		}
 
-		if (health == 0)
-		{
-			Destroy(defender);
-			foreach (GameObject g in attackers)
-			{
-				if (g) Destroy(g);
+		if (health == 0) {
+			print ("Game over, attackers win!");
+			Destroy (defender);
+			foreach (AttackerWithTarget g in attackers) {
+				if (g.attacker)
+					Destroy (g.attacker);
 			}
 			enabled = false;
 		}
@@ -84,22 +85,27 @@ public class ProtectGame : MonoBehaviour {
 	{
 		Vector3 currentLocation = a.pos;
 
-		bool vertical = (currentLocation.x == targetPos.x);
-		bool above = currentLocation.y > targetPos.y ? true : false;
-
-		if (vertical && above)
-			return new Vector3 (targetPos.x, 20, 1);
-		else if (vertical && !above)
-			return new Vector3 (targetPos.x, -20, 1);
+		if (currentLocation.x == targetPos.x) { // In a vertical line
+			if (currentLocation.y > targetPos.y) // Attacker is above target
+				return new Vector3 (targetPos.x, 20, 1);
+			else // Attacker is below target
+				return new Vector3 (targetPos.x, -20, 1);
+		}
 
 		bool left = currentLocation.x < targetPos.x ? true : false;
 
+		if (currentLocation.y == targetPos.y) { // In a horizontal line
+			if (left) // Attacker is to the left of the target
+				return new Vector3 (-20, targetPos.y, 1);
+			else // Attacker is to the right of the target
+				return new Vector3 (20, targetPos.y, 1);
+		}
+
 		float slope = (targetPos.y - currentLocation.y) / (targetPos.x - currentLocation.x);
 
-		if (left) {
+		if (left)
 			return new Vector3 (-20, 1 - (20 * slope), 1);
-		} else { // right
+		else // right
 			return new Vector3 (20, 1 + (20 * slope), 1);
-		}
 	}
 }
